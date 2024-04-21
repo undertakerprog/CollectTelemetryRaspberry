188122ee4bfdd4d34267ea2cef7071cb079bcb38
MaximSolodkovVMSIS <112771644+MaximSolodkovVMSIS@users.noreply.github.com>
2024-04-21 12:19:49 +0100

    Delete src directory

--- src/json.h
+++
@@ -1,5 +0,0 @@
-#include <cjson/cJSON.h>
-
-char *serialize_to_JSON(float temperature, const char *memory_info, float humidity, float dht11_temperature, int sound_value);
-
-char *serialize_to_JSON_without_sencors(float temperature, const char *memory_info, int sound_value);
