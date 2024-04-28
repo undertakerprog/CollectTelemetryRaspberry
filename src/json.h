363668c31c53c3122239479857d05a2726eed0aa
MaximSolodkovVMSIS <112771644+MaximSolodkovVMSIS@users.noreply.github.com>
2024-04-28 18:01:31 +0100

    Delete src directory

--- src/json.h
+++
@@ -1,6 +0,0 @@
-#include <cjson/cJSON.h>
-#include <stdio.h>
-
-char *serialize_to_JSON(float temperature, const char *memory_info, float humidity, float dht11_temperature, int sound_value, int is_file);
-
-char *serialize_to_JSON_without_sencors(float temperature, const char *memory_info, int sound_value);
--- src/send_data.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#define SENSOR_CHECK_DELAY_MS 5000
-#define DATA_FILE "sensor_data.json"
-
-void send_file_to_computer();
